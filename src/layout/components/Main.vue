fd212add75f890906be004e86bfbef37a389d85e
Lorenzo <wangjp@jiguang.cn>
2022-06-15 15:45:02 +0800

    feat: fix

--- src/layout/components/Main.vue
+++ src/layout/components/Main.vue
@@ -1,2 +1,15 @@
 <template>
+  <RouterView>
+    <template #default="{ Component, route }">
+      
+    </template>
+  </RouterView>
 </template>
+
+<script setup lang="ts">
+
+</script>
+
+<style lang="less" scoped>
+
+</style>
