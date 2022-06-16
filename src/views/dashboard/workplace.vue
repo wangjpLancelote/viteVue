7c21dd12401058e42d096c448cb1ea4f924cc97d
Lorenzo <wangjp@jiguang.cn>
2022-06-16 12:24:28 +0800

    feat: fix

--- src/views/dashboard/workplace.vue
+++ src/views/dashboard/workplace.vue
@@ -1,9 +1,12 @@
 <template>
   <div>
-    <n-grid cols="1 s:2 m:3 l:4 xl:4 2xl:4" responsive="screen" :x-gap="12" :y-gap="8"></n-grid>
+    <n-grid cols="1 s:2 m:3 l:4 xl:4 2xl:4" responsive="screen" :x-gap="12" :y-gap="8">
+      workspace
+    </n-grid>
   </div>
 </template>
 <script lang="ts" setup>
+import { onMounted } from 'vue';
 import { useDialog } from "naive-ui";
 onMounted(() => {
 
